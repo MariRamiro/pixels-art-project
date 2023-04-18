87162eb547756597e5fcf29b48285e1b93e907be
Mariana Ramiro <marianaramiroof@gmail.com>
2023-04-18 00:54:55 -0300

    requisito 1

---
+++ script.js
@@ -0,0 +1,10 @@
+const tableHead = document.getElementsByClassName('color');
+
+for let
+
+
+
+const black = document.getElementsByClassName('color')[0];
+const red = document.getElementsByClassName('color')[1];
+const blue = document.getElementsByClassName('color')[2];
+const green = document.getElementsByClassName('color')[3];
