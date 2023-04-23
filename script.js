cb05ed12c981505afa37452e5789aede0d90c791
Mariana Ramiro <marianaramiroof@gmail.com>
2023-04-23 16:39:35 -0300

    desafios 4 e 5

--- script.js
+++ script.js
@@ -14,6 +14,7 @@ document.getElementById('color3').style.backgroundColor = 'green';
 document.getElementById('color4').style.backgroundColor = 'blue';
 
 const changeColor = () => {
+const savedColor = ['black'];
 for(let index = 1; index < paletteList.length; index +=1) {
     const colors = document.getElementsByClassName('color');
     let r = Math.floor(Math.random() * 255);
@@ -21,9 +22,20 @@ for(let index = 1; index < paletteList.length; index +=1) {
     let b = Math.floor(Math.random() * 255);
 
   colors[index].style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
+  savedColor.push(`rgb(${r}, ${g}, ${b})`);
 }
+
+localStorage.setItem('colorPalette', JSON.stringify(savedColor));
 };
 
 const btnChangeColor = document.getElementById("button-random-color");
 
 btnChangeColor.addEventListener('click', changeColor);
+
+const colorPalette = JSON.parse(localStorage.getItem('colorPalette'));
+
+// selecionar o local de destino
+for(let index = 1; index < colorPalette.length; index +=1){
+    palette.children[index].style.backgroundColor = colorPalette[index]
+};
+
