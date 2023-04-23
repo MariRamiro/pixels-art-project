3dc1830ec616c1f8dab9dd2306b65e34184b243a
Mariana Ramiro <marianaramiroof@gmail.com>
2023-04-23 12:29:52 -0300

    desafio 2

--- script.js
+++ script.js
@@ -1,45 +1,16 @@
+const palette = document.getElementById("color-palette");
 
-const palette = document.getElementById('color-palette');
-
-const chooseColor = () => {
-    for(let i=0; i < 4; i +=1){
-        const button = document.getElementsByClassName('.color');
-        button.appendChild(palette);
-    }
+const paletteHead = document.getElementsByClassName("color");
+for(let index = 0; index < paletteHead.length; index +=1){
+    const paletteTitle = [];
+    paletteTitle.push(paletteHead[index]);
 };
-chooseColor();
-const button = document.querySelectorAll('.color');
 
-button[0].style.backgroundColor = 'black';
-button[1].style.backgroundColor = 'red';
-button[2].style.backgroundColor = 'blue';
-button[3].style.backgroundColor = 'green';
+let btns = document.querySelectorAll('button')
+for(button of btns) {
+    let r = Math.floor(Math.random() * 255),
+        g = Math.floor(Math.random() * 255),
+        b = Math.floor(Math.random() * 255);
 
-
-
-
-
-
-
-
-
-
-    //const table = document.createElement('table');
-    //const thead = document.createElement('thead');
-    //const tbody = document.createElement('tbody');
-
-    
-
-    //const thd = (i) => {
-   //     for(let i = 0; i<content.length; i+=1);{
-    //        let tr = document.createElement('tr');
-   
-
-// referencia https://pt.stackoverflow.com/questions/49581/criar-tabela-din%C3%A2mica-em-js-para-utilizar-em-html
-// https://www.delftstack.com/pt/howto/javascript/create-table-javascript/
-
-
-//const black = document.getElementsByClassName('color')[0];
-//const red = document.getElementsByClassName('color')[1];
-//const blue = document.getElementsByClassName('color')[2];
-//const green = document.getElementsByClassName('color')[3];
+  button.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
+}
