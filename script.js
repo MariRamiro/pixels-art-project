2eef1c7ff07dbe3ca62302dcccd01f99895ea75b
Mariana Ramiro <marianaramiroof@gmail.com>
2023-04-23 19:16:04 -0300

    desafio 6

--- script.js
+++ script.js
@@ -40,3 +40,10 @@ if (colorPalette !== null) {
 }
 
 const board = document.getElementById('pixel-board');
+for(let index =0; index < 25; index +=1){
+    const pixel = document.createElement('div')
+    pixel.className = 'pixel';
+    pixel.style.backgroundColor = 'white';
+    board.appendChild(pixel);
+}
+
