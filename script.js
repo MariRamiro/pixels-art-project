8b95d1352e3c8cf5e05fdaf852e39e3e9ded136b
Mariana Ramiro <marianaramiroof@gmail.com>
2023-04-23 17:18:31 -0300

    desafio 5

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const paletteHead = document.getElementsByClassName("color");
 
 const paletteList = [];
 
-for(let index = 0; index < 4; index +=1){
+for (let index = 0; index < 4; index += 1) {
     paletteList.push(paletteHead[index]);
 };
 
@@ -14,18 +14,18 @@ document.getElementById('color3').style.backgroundColor = 'green';
 document.getElementById('color4').style.backgroundColor = 'blue';
 
 const changeColor = () => {
-const savedColor = ['black'];
-for(let index = 1; index < paletteList.length; index +=1) {
-    const colors = document.getElementsByClassName('color');
-    let r = Math.floor(Math.random() * 255);
-    let g = Math.floor(Math.random() * 255);
-    let b = Math.floor(Math.random() * 255);
+    const savedColor = ['black'];
+    for (let index = 1; index < paletteList.length; index += 1) {
+        const colors = document.getElementsByClassName('color');
+        let r = Math.floor(Math.random() * 255);
+        let g = Math.floor(Math.random() * 255);
+        let b = Math.floor(Math.random() * 255);
 
-  colors[index].style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
-  savedColor.push(`rgb(${r}, ${g}, ${b})`);
-}
+        colors[index].style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
+        savedColor.push(`rgb(${r}, ${g}, ${b})`);
+    }
 
-localStorage.setItem('colorPalette', JSON.stringify(savedColor));
+    localStorage.setItem('colorPalette', JSON.stringify(savedColor));
 };
 
 const btnChangeColor = document.getElementById("button-random-color");
@@ -33,9 +33,10 @@ const btnChangeColor = document.getElementById("button-random-color");
 btnChangeColor.addEventListener('click', changeColor);
 
 const colorPalette = JSON.parse(localStorage.getItem('colorPalette'));
+if (colorPalette !== null) {
+    for (let index = 1; index < colorPalette.length; index += 1) {
+        palette.children[index].style.backgroundColor = colorPalette[index]
+    }
+}
 
-// selecionar o local de destino
-for(let index = 1; index < colorPalette.length; index +=1){
-    palette.children[index].style.backgroundColor = colorPalette[index]
-};
-
+const board = document.getElementById('pixel-board');
